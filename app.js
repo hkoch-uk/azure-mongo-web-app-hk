445fabac69a3c715c45dba7562dfc6fbb5cf93d7
Dylmay <36193755+Dylmay@users.noreply.github.com>
2022-09-28 18:48:08 +0100

    set port to 3000

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
 });
 
-const port = process.env.PORT || 3200;
+const port = process.env.PORT || 3000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
