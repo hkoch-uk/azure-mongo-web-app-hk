4635fe66b49f2ab1b871480558a09fdca767e432
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2022-09-28 22:51:34 +0100

    add css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import Register from "./components/Register";
+import SchoolPage from "./pages/SchoolPage";
 
 function App() {
   return (
     <div>
-   
-          <Register />
+      {/* <Register /> */}
+      <SchoolPage />
     </div>
   );
 }
--- frontend/src/pages/SchoolPage.jsx
+++ frontend/src/pages/SchoolPage.jsx
@@ -2,16 +2,50 @@ import { useEffect, useState } from "react";
 
 //fetch data about childrens
 // patch the data to the backend
-
+const mockChilrenData = [
+  { name: "linlin", hobby: ["football", "drawing"], age: 5, mentor: "kim" },
+  { name: "Ryan", hobby: ["football", "drawing"], age: 6, mentor: "kim" },
+  { name: "Luqman", hobby: ["football", "drawing"], age: 7, mentor: "kim" },
+];
 export default function SchoolPage() {
-  const [children, setChildren] = useState({});
+  const [children, setChildren] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    setChildren(mockChilrenData);
+  }, []);
+
+  const fetchChildrenData = () => {
+    fetch(``)
+      .then((childrenFromDatabase) => childrenFromDatabase.json())
+      .then((childrenFromDatabase) => setChildren(childrenFromDatabase));
+  };
 
   return (
     <div className="school-page">
       <section>
         <button>View Chilren</button>
+        <section className="child-view-section">
+          {children.length ? (
+            <>
+              {children.map((child) => {
+                console.log("child", child);
+                return (
+                  <div className="child-card">
+                    <p>Name:{child.name}</p>
+                    <p>
+                      hobby:
+                      {child.hobby.map((hobby) => (
+                        <span className="hobby-tag">{hobby}</span>
+                      ))}
+                    </p>
+                    <p>age:{child.age}</p>
+                    <p>mentor:{child.mentor}</p>
+                  </div>
+                );
+              })}
+            </>
+          ) : null}
+        </section>
       </section>
     </div>
   );
