6e9ca4c6c4322f2ffb66db07ea95cbe85207f00f
francescamatthews <fj.matthews@outlook.com>
2022-09-28 23:58:54 +0100

    form styling

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -11,10 +11,9 @@ function Register() {
 
 
     return<>
-<div class="box">
-  Explain memberships/info
-  Individual child register is after signing up as school
-</div>
+    <div class="column is-three-fifths
+is-offset-one-fifth">
+    <div class="container">
 
 <div class="field">
   <label class="label">Type</label>
@@ -37,28 +36,34 @@ function Register() {
 </div>
 <div class="field">
   <label class="label">Address</label>
-  <div class="control">
+  <div class="control has-icons-left">
     <input class="input" type="text" placeholder="Address" /> 
+    <span class="icon is-small is-left">
+    <i class="fas fa-house"></i>
+    </span>
   </div>
 </div>
 
 <div class="field">
   <label class="label">Email</label>
   <div class="control has-icons-left has-icons-right">
-    <input class="input is-danger" type="email" placeholder="Email address" /> 
+    <input class="input" type="email" placeholder="Email address" /> 
     <span class="icon is-small is-left">
       <i class="fas fa-envelope"></i>
     </span>
-    <span class="icon is-small is-right">
+    {/* <span class="icon is-small is-right">
       <i class="fas fa-exclamation-triangle"></i>
-    </span>
+    </span> */}
   </div>
   {/* <p class="help is-danger">This email is invalid</p> */}
 </div>
 <div class="field">
   <label class="label">Contact Number</label>
-  <div class="control">
+  <div class="control has-icons-left">
     <input class="input" type="text" placeholder="Contact Number" /> 
+    <span class="icon is-small is-left">
+      <i class="fas fa-phone"></i>
+    </span>
   </div>
 </div>
 
@@ -66,40 +71,11 @@ function Register() {
 
   <div class="field">
   <label class="label">Occupation</label>
-  <div class="control">
-    <textarea class="textarea" placeholder="Occupation"></textarea>
-  </div>
-</div>
-
-<div class="field">
-  <label class="label">Interests (Control and click up to 3 options)</label>
-  <div class="control">
-    <div class="select is-multiple">
-      <select multiple size="3">
-        <option>Sports</option>
-        <option>Arts and Crafts</option>
-        <option>TV and Film</option>
-        <option>Reading</option>
-        <option>Outdoors</option>
-      </select>
-    </div>
-  </div>
-</div>
-
-<div class="field">
-  <div class="control">
-    <label class="checkbox">
-      <input type="checkbox" />  
-      I agree to the <a href="#">sending a letter twice a term</a>
-    </label>
-  </div>
-</div>
-  
-
-<div class="field">
-  <label class="label">Occupation</label>
-  <div class="control">
-    <textarea class="textarea" placeholder="Occupation"></textarea>
+  <div class="control has-icons-left">
+    <input class="input" type="text" placeholder="Occupation"></input>
+    <span class="icon is-small is-left">
+      <i class="fas fa-briefcase"></i>
+    </span>
   </div>
 </div>
 
@@ -132,7 +108,7 @@ function Register() {
 <div class="field">
   <label class="label">School Name</label>
   <div class="control">
-    <textarea class="textarea" placeholder="School Name"></textarea>
+    <input class="input" type="text" placeholder="School Name"></input>
   </div>
 </div>
 
@@ -140,7 +116,7 @@ function Register() {
 <div class="field">
   <label class="label">Area child would like support with</label> 
   <div class="control">
-    <textarea class="textarea" placeholder="Area for support"></textarea>
+    <input class="input" type="text" placeholder="Area for support"></input>
   </div>
 </div>
 
@@ -156,7 +132,7 @@ function Register() {
 <div class="field">
   <label class="label">Sign</label>
   <div class="control">
-    <textarea class="textarea" placeholder="Sign here"></textarea>
+    <input class="input" type="text" placeholder="Sign here"></input>
   </div>
 </div>
 
@@ -175,6 +151,8 @@ function Register() {
     <button class="button is-link is-light">Cancel</button>
   </div>
 </div>
+  </div>
+  </div>
     </>
 
 
